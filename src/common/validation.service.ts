b8384fa21d5c3f4b4591674d561e2ec85f0530f8
AndreaSuryatanaya <andreasemut@gmail.com>
2025-09-02 01:44:17 +0700

    create user sercvie - register

--- src/common/validation.service.ts
+++ src/common/validation.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { ZodType } from 'zod';
 
+@Injectable()
 export class ValidationService {
   validate<T>(zodType: ZodType<T>, data: T): T {
     return zodType.parse(data);
--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -1,18 +1,23 @@
 import { z, ZodType } from 'zod';
+import {
+  RegisterUserRequest,
+  LoginUserRequest,
+  UpdateUserRequest,
+} from '../model/user.model';
 
 export class UserValidation {
-  static readonly REGISTER: ZodType = z.object({
+  static readonly REGISTER: ZodType<RegisterUserRequest> = z.object({
     username: z.string().min(1).max(100),
     password: z.string().min(1).max(100),
     name: z.string().min(1).max(100),
   });
 
-  static readonly LOGIN: ZodType = z.object({
+  static readonly LOGIN: ZodType<LoginUserRequest> = z.object({
     username: z.string().min(1).max(100),
     password: z.string().min(1).max(100),
   });
 
-  static readonly UPDATE: ZodType = z.object({
+  static readonly UPDATE: ZodType<UpdateUserRequest> = z.object({
     name: z.string().min(1).max(100).optional(),
     password: z.string().min(1).max(100).optional(),
   });
